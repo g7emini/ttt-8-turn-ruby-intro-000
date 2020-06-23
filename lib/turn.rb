d3e0edc30c84d023c69582f67191a1204ef40044
groana <contact@groanamelendez.com>
2020-06-23 02:17:48 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,4 @@
-# board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ","   ", "   "]
-
-# def display_board(arr)
-#   puts "   |   |   "
-#   puts "-----------"
-#   puts "   |   |   "
-#   puts "-----------"
-#   puts "   |   |   "
-# end
-
+board = ["   ", "   ", "   ", "   ", "   ", "   ", "   ","   ", "   "]
 
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
@@ -15,10 +6,7 @@ def display_board(board)
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def display_board
-  
+  puts board
 end
 
 
