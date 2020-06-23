0373e82a364891b51a4060447a488dbfead9c475
groana <contact@groanamelendez.com>
2020-06-23 02:17:04 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,17 @@
+def display_board
+  
+end
+
+
+def valid_move?
+  
+end
+
+# ask for input
+# get input
+# convert input to index
+# if index is valid 
+# make the move for index show the board
+# else
+#   ask for input again until you get a valid input
+# end
